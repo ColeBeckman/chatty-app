d88f70ead44996a36bdeeb8974c0691996074942
cole <colebeckman63@gmail.com>
2024-02-28 18:40:04 -0600

    Added dynamic routing and redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/General',
+        permanent: true,
+      },
+    ];
+  },
   experimental: {
     serverComponentsExternalPackages: ['knex'],
   },
--- src/components/chat-rooms/chat-room.tsx
+++ src/components/chat-rooms/chat-room.tsx
@@ -1,11 +1,16 @@
 import { ChatRoomType } from '@/types';
+import Link from 'next/link';
 
 interface Props {
   room: ChatRoomType;
 }
 const ChatRoom = (props: Props) => {
   const { room } = props;
-  return <li>{room.room_name}</li>;
+  return (
+    <li>
+      <Link href={`/${room.room_name}`}>{room.room_name}</Link>
+    </li>
+  );
 };
 
 export default ChatRoom;
